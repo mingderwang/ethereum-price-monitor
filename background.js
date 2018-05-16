345d562c4f8298e83882e48f8a0d2383e3e26678
pcunningham <pcunningham@spotx.tv>
2018-05-16 17:52:59 +0100

    Added app store image.

--- background.js
+++ background.js
@@ -1,31 +1,46 @@
 var lastPrice = 0;
 
+
+function updatePrice() {
+
+	chrome.storage.sync.get(['currency'], function(data) {
+
+			var currency = data.currency || 'USD';
+
+			var xhr = new XMLHttpRequest();
+
+			xhr.open("GET", "https://api.gdax.com/products/eth-" + currency.toLowerCase() + "/ticker", true);
+
+			xhr.onreadystatechange = function() {
+			  if (xhr.readyState == 4) {
+
+			    var resp = JSON.parse(xhr.responseText);
+
+			    chrome.browserAction.setTitle({title: resp.price });
+
+			    chrome.browserAction.setBadgeText({text:resp.price.substring(0,4)});
+
+			    chrome.browserAction.setBadgeBackgroundColor({
+			    	color : resp.price >= lastPrice ? "green" : "red"
+			    });
+
+			    lastPrice = resp.price;
+			  }
+			}
+			
+			xhr.send();
+	});
+}
+
 function updateData() {
 
-	var xhr = new XMLHttpRequest();
+	try { updatePrice(); } catch(e) {}
 
-	xhr.open("GET", "https://api.gdax.com/products/eth-usd/ticker", true);
+	window.setTimeout(updateData, 1000 * 5);
 
-	xhr.onreadystatechange = function() {
-	  if (xhr.readyState == 4) {
 
-	    var resp = JSON.parse(xhr.responseText);
-
-	    chrome.browserAction.setTitle({title: resp.price });
-
-	    chrome.browserAction.setBadgeText({text:resp.price.substring(0,4)});
-
-	    chrome.browserAction.setBadgeBackgroundColor({
-	    	color : resp.price >= lastPrice ? "green" : "red"
-	    });
-
-	    lastPrice = resp.price;
-	  }
-	}
-	
-	xhr.send();
-
-	window.setTimeout(updateData, 1000 * 10);
+	chrome.storage.sync.onChange(updatePrice);
 }
 
 updateData();
+
--- popup.js
+++ popup.js
@@ -1,12 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var links = document.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        (function () {
-            var ln = links[i];
-            var location = ln.href;
-            ln.onclick = function () {
-                chrome.tabs.create({active: true, url: location});
-            };
-        })();
-    }
+
+	chrome.storage.sync.get(['currency'], function(data) {
+
+		  var currency = data.currency || 'USD';
+
+		  var radio = document.getElementById('currency' + currency.toUpperCase()).checked = true;
+
+		  var links = document.getElementsByTagName("a");
+
+		    for (var i = 0; i < links.length; i++) {
+		        (function () {
+		            var ln = links[i];
+		            var location = ln.href;
+		            ln.onclick = function () {
+		                chrome.tabs.create({active: true, url: location});
+		            };
+		        })();
+		    }
+
+		    var radios = document.getElementsByTagName("input");
+
+		    for (i = 0; i < radios.length; i++) {
+		    	(function () {
+		    			var rad = radios[i];
+			    		rad.onclick = function() {
+			    			 chrome.storage.sync.set({
+							    currency: document.querySelector(
+							    	'input[name="currency"]:checked'
+							    ).value
+							 });
+			    		}
+		        })();
+		    }
+
+	});
+
+  
+
 });
